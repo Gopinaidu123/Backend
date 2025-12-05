87a3889bb9f76b7b07049737de67d6cd2b3890b9
Gopinaidu123 <gopinaidu043@gmail.com>
2025-12-05 11:33:39 +0530

    refactor: migrate from http to express for improved server handling

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-import http from 'http';
-import { json } from 'stream/consumers';
-const server =http.createServer((req,res) => {
-    console.log(req.method);
-    if(req.url=="/user") {
-        res.writeHead(200,{"content-Type" : "application/json"});
-        res.end(JSON.stringify({name:"thub"}))
-    } else{
-        res.writeHead(200,{"content-Type" : "text/plain"});
-        res.end("Hello this is from Backend aha")
-    }
+// import http from 'http';
+// import { json } from 'stream/consumers';
+// const server =http.createServer((req,res) => {
+//     console.log(req.method);
+//     if(req.url=="/user") {
+//         res.writeHead(200,{"content-Type" : "application/json"});
+//         res.end(JSON.stringify({name:"thub"}))
+//     } else{
+//         res.writeHead(200,{"content-Type" : "text/plain"});
+//         res.end("Hello this is from Backend aha")
+//     }
 
-});
-server.listen(7007 , () =>{
-    console.log(`server running at port ${7007}`)
+// });
+// server.listen(7007 , () =>{
+//     console.log(`server running at port ${7007}`)
+// });
+
+import express from 'express';
+const app = express();
+app.get("/users", (req, res) => {
+    res.send("Hello from Express server");
+})
+app.get("/get-user", (req, res) => {
+    req.send("User data from Express server");
+})
+app.listen(7007, () => {
+    console.log("Express server running at port 7007");
 });
