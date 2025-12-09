b3e00136b5cc03ddef2a96fa5e7df7ff1e17be99
Gopinaidu123 <gopinaidu043@gmail.com>
2025-12-09 10:05:36 +0530

    refactor: clean up middleware usage in index.js

--- index.js
+++ index.js
@@ -18,11 +18,12 @@
 import express from 'express';
 const app = express();
 
- 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();  
-});
+app.use(express.json());
+
+// app.use((req, res, next) => {
+//     console.log(`${req.method} ${req.url}`);
+//     next();  
+// });
  
 app.use(express.json());
 
