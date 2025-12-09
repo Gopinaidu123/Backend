34a48e3de53976aa7922ca1ceb76b44b152b19d6
Gopinaidu123 <gopinaidu043@gmail.com>
2025-12-09 09:52:53 +0530

    fix: update response message in /users endpoint

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.get("/users", (req, res) => {
-    res.send("Hello due");
+    res.send("Hello bro");
 });
 
  app.post("/data", (req, res) => {
