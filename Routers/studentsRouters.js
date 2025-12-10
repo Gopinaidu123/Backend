66e75c12a71d82146661eab2f16743dd81bd5b46
Gopinaidu123 <gopinaidu043@gmail.com>
2025-12-10 11:31:18 +0530

    fix: refactor students router to correct import paths and update endpoint names

--- Routers/studentsRouters.js
+++ Routers/studentsRouters.js
@@ -1,7 +1,14 @@
-import experss from "express";
-import { getStudents, addStudents } from "../controllers/studentsController.js";  
-const router = experss.Router();
+// import experss from "express";
+// import { getStudents, addStudents } from "../controllers/studentsController.js";  
+// const router = experss.Router();
 
-router.get("/get-students", getStudents);
-router.post("/add-students", addStudents);
+// router.get("/get-students", getStudents);
+// router.post("/add-students", addStudents);
+// export default router;
+
+import express from'express';
+import { getStudents,addstudents } from '../controller/studentsController.js';
+const router = express.Router();
+router.get('/get-student' ,getStudents);
+router.post('/add-student' ,addstudents);
 export default router;
