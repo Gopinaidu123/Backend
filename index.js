26839404a4f1e617dc017c23d028aacab0aec272
Gopinaidu123 <gopinaidu043@gmail.com>
2025-12-10 11:09:04 +0530

    fix: correct import path for student router in index.js

--- index.js
+++ index.js
@@ -17,15 +17,12 @@
 
 import express from 'express';
 const app = express();
-import studentRouter from './controller/Routers/stuudentsRouters.js';
+import studentRouter from './Routers/stuudentsRouters.js';
 
 
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//     console.log(`${req.method} ${req.url}`);
-//     next();  
-// });
+ 
  
 app.use(express.json());
 
